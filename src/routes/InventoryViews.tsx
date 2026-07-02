310ce733fb5e3ee88173576e4f1d2852f5bb91d8
Zhamilya Abikenova <zabikeno@redhat.com>
2026-07-02 14:28:06 +0200

    fix: hide Global tag filter when SystemsView is enabled

--- src/routes/InventoryViews.tsx
+++ src/routes/InventoryViews.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useLayoutEffect, useState } from 'react';
 import { Flex, FlexItem, PageSection } from '@patternfly/react-core';
 import SystemsView from '../components/SystemsView';
 import {
@@ -10,6 +10,8 @@ import { OutageAlert } from '../components/OutageAlert';
 import SystemsViewToggle from '../components/SystemsView/SystemsViewToggle';
 import { AccountStatContext } from '../Contexts';
 import { ImagesView } from '../components/InventoryViews/ImagesView/ImagesView';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import useSystemsViewFeatureFlag from '../Utilities/useSystemsViewFeatureFlag';
 import './inventory.scss';
 
 interface InventoryViewsProps {
@@ -21,6 +23,18 @@ export type View = 'systems' | 'images';
 const InventoryViews = ({ hasAccess }: InventoryViewsProps) => {
   const [view, setView] = useState<View>('systems');
   const { hasBootcImages } = useContext(AccountStatContext);
+  const chrome = useChrome();
+  const isSystemsViewEnabled = useSystemsViewFeatureFlag();
+
+  useLayoutEffect(() => {
+    if (isSystemsViewEnabled) {
+      chrome?.hideGlobalFilter?.(true);
+    }
+
+    return () => {
+      chrome?.hideGlobalFilter?.(false);
+    };
+  }, [isSystemsViewEnabled, chrome]);
 
   return (
     <>
